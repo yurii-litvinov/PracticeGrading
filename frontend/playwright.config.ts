e5d905f17abd143784e901cd1782f9669f86ee94
mawekk <mariamya243@gmail.com>
2024-12-21 04:09:00 +0300

    fix: set firefox headless mode

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -41,10 +41,7 @@ export default defineConfig({
 
     {
       name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-        headless: false,
-      },
+      use: { ...devices['Desktop Firefox'] },
     },
 
     {
